4ec4ebf23f9832f1c35fe56d30a7e290e3e918c4
Shane <Shane@Shane-PC>
2015-05-06 01:30:27 +0100

    Added line counter

---
+++ Misc Small Projects/src/ga/shane/misc/LineCounter.java
@@ -0,0 +1,38 @@
+package ga.shane.misc;
+
+import ga.shane.utilities.ArrayUtils;
+import ga.shane.utilities.FileUtils;
+
+import java.io.File;
+
+/** 
+ * @author http://www.shane.ga
+ */
+public class LineCounter {
+	static int total = 0;
+	static File baseDir = new File("C:/Users/Shane/git/Global-Utilities/Global Utilities");
+	static boolean path = false;
+	static String[] acceptedExtensions = {
+		"java"
+	};
+	
+	public static void main(String[] args) {
+		scan(baseDir);
+		System.out.println("\nTotal lines: " + total);
+	}
+	
+	static void scan(File dir) {
+		for(File file : dir.listFiles()) {
+			if(file.isDirectory())
+				scan(file);
+			else {
+				if(!(acceptedExtensions.length == 0 ? true : ArrayUtils.contains(acceptedExtensions, file.getName().split("\\.")[1])))
+					continue;
+				
+				int size = FileUtils.getFileLines(file).size();
+				total+= size;				
+				System.out.println((path ? file.getPath() : file.getName()) + ": " + size + " lines.");
+			}
+		}
+	}
+}
